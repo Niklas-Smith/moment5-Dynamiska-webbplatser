acf186b98db91772d155eaa7f377eaafe34535ff
Niklas Smith <niklas_smith@hotmail.com>
2025-05-12 19:22:17 +0200

    Fetch in api

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,51 @@
+ 
+ let admission = [];
+ 
+ 
+ window.onload = () => {
+    
+getAdmissionInfo()
+    }
+ 
+ 
+ async function getAdmissionInfo()  {
+try {
+
+const resp = await fetch ("https://studenter.miun.se/~mallar/dt211g/")
+
+
+if(!resp.ok) {
+ throw new error("Kan ej läsa in")
+    
+ }
+
+admission = await resp.json();
+
+
+WriteDiagram()
+
+
+ }catch(error) {
+ console.error(error); }
+ 
+
+ 
+ 
+}
+
+function WriteDiagram() {
+let test = admission
+
+
+ 
+
+
+let test2 = test.sort((a,b) => b.applicantsTotal-a.applicantsTotal).slice(0,6);
+
+console.table(test2);
+
+
+
  const ctx = document.getElementById('myChart');
 
 
@@ -12,6 +60,10 @@
       }]
     },
   });
+
+}
+
+
 
 
   
